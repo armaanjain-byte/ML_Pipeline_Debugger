b753f4d8b488dcca054e748a05b1f4423ef6ec73
armaan jain <armaanjain.work00@gmail.com>
2026-05-23 14:56:39 +0530

    fixed container block

--- dashboard.py
+++ dashboard.py
@@ -58,7 +58,7 @@ st.markdown(
     }
 
     .block-container {
-        padding-top: 1.5rem;
+        padding-top: 5rem;
         padding-bottom: 2rem;
         padding-left: 2rem;
         padding-right: 2rem;
